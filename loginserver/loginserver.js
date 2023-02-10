131b6ff196e0f42d84781fe74923614bc16bebb6
Will Schulz <wschulz@princeton.edu>
2023-02-10 14:22:57 -0500

    simplify loginserver

--- loginserver/loginserver.js
+++ loginserver/loginserver.js
@@ -4,36 +4,15 @@ var express = require('express');
 var app = express();
 var fs = require('fs');
 
-app.get('/loginserver/listall', function (req, res) {
-   fs.readFile( __dirname + "/" + "a.json", 'utf8', function (err, data) {
+app.get('/loginserver/listall/:db', function (req, res) {
+   fs.readFile( __dirname + "/database/" + req.params.db + ".json", 'utf8', function (err, data) {
       console.log( data );
       res.end( data );
    });
 })
 
 
-//app.get('/loginserver/getinfo/:id', function (req, res) {
-//   // First read existing users.
-//   fs.readFile( __dirname + "/" + "a.json", 'utf8', function (err, data) {
-//      var users = JSON.parse( data );
-//      var user = users["user" + req.params.id]
-//      console.log( user );
-//      res.end( JSON.stringify(user));
-//   });
-//})
-
-app.get('/loginserver/getinfo/:id', function (req, res) {
-   // First read existing users.
-   fs.readFile( __dirname + "/" + "participant_data.json", 'utf8', function (err, data) {
-      var users = JSON.parse( data );
-      var user = users[req.params.id]
-      console.log( user );
-      res.end( JSON.stringify(user));
-   });
-})
-
-
-app.get('/loginserver/getinfocustom/:db/:id', function (req, res) {
+app.get('/loginserver/getinfo/:db/:id', function (req, res) {
    // First read existing users.
    fs.readFile( __dirname + "/database/" + req.params.db + ".json", 'utf8', function (err, data) {
       var users = JSON.parse( data );
@@ -43,64 +22,41 @@ app.get('/loginserver/getinfocustom/:db/:id', function (req, res) {
    });
 })
 
-
-app.get('/loginserver/getinfo_w3/:id', function (req, res) {
-   // First read existing users.
-   fs.readFile( __dirname + "/" + "participant_data_w3.json", 'utf8', function (err, data) {
-      var users = JSON.parse( data );
-      var user = users[req.params.id]
-      console.log( user );
-      res.end( JSON.stringify(user));
-   });
-})
-
-
-//app.get('/loginserver/getassignments/:id', function (req, res) {
-//   // First read existing users.
-//   fs.readFile( __dirname + "/" + "a.json", 'utf8', function (err, data) {
-//      var users = JSON.parse( data );
-//      var user = users["user" + req.params.id]
-//      var assignments = user["assignments"]
-//      console.log( assignments );
-//      res.end( JSON.stringify(assignments));
-//   });
-//})
-
-app.get('/loginserver/getassignments/:id', function (req, res) {
-   // First read existing users.
-   fs.readFile( __dirname + "/" + "test.json", 'utf8', function (err, data) {
-      var users = JSON.parse( data );
-      var user = users[req.params.id]
-      var assignments = user["assignments"]
-      console.log( assignments );
-      res.end( JSON.stringify(assignments));
-   });
-})
-
-app.get('/loginserver/getinfotest', function (req, res) {
-   // First read existing users.
-   fs.readFile( __dirname + "/" + "a.json", 'utf8', function (err, data) {
-      var users = JSON.parse( data );
-      var user = users["user1"]
-      console.log( user );
-      res.end( JSON.stringify(user));
-   });
-})
+// app.get('/loginserver/getassignments/:id', function (req, res) {
+//    // First read existing users.
+//    fs.readFile( __dirname + "/" + "test.json", 'utf8', function (err, data) {
+//       var users = JSON.parse( data );
+//       var user = users[req.params.id]
+//       var assignments = user["assignments"]
+//       console.log( assignments );
+//       res.end( JSON.stringify(assignments));
+//    });
+// })
+//
+// app.get('/loginserver/getinfotest', function (req, res) {
+//    // First read existing users.
+//    fs.readFile( __dirname + "/" + "a.json", 'utf8', function (err, data) {
+//       var users = JSON.parse( data );
+//       var user = users["user1"]
+//       console.log( user );
+//       res.end( JSON.stringify(user));
+//    });
+// })
+//
+//
+// app.get('/loginserver/getassignmentstest', function (req, res) {
+//    // First read existing users.
+//    fs.readFile( __dirname + "/" + "a.json", 'utf8', function (err, data) {
+//       var users = JSON.parse( data );
+//       var user = users["user1"]
+//       var assignments = user["assignments"]
+//       console.log( assignments );
+//       res.end( JSON.stringify(assignments));
+//    });
+// })
 
 
-app.get('/loginserver/getassignmentstest', function (req, res) {
-   // First read existing users.
-   fs.readFile( __dirname + "/" + "a.json", 'utf8', function (err, data) {
-      var users = JSON.parse( data );
-      var user = users["user1"]
-      var assignments = user["assignments"]
-      console.log( assignments );
-      res.end( JSON.stringify(assignments));
-   });
-})
-
-
-app.get('/loginserver/test1', function (req, res) {
+app.get('/loginserver/test', function (req, res) {
    // very simple test
    res.status(200).send("WHATABYTE: Food For Devs");
 })
